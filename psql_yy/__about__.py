8e06536333e189dbee4a79a64cabcbe0a2237a1d
yy <1023767856@qq.com>
2019-09-21 22:32:26 +0800

    initialize

---
+++ psql_yy/__about__.py
@@ -0,0 +1,16 @@
+"""
+Create by yy on 2019/9/21
+"""
+
+__title__ = 'psql-yy'
+__description__ = 'A tool which is used to connect postgresql, designed by yy'
+__url__ = 'https://github.com/guaidashu/psql_yy'
+__version_info__ = ('0', '1', '3')
+__version__ = '.'.join(__version_info__)
+__author__ = 'guaidashu'
+__author_email__ = 'song42960@gmail.com'
+__maintainer__ = 'YY blog'
+__license__ = 'MIT'
+__copyright__ = '(c) 2019 by guaidashu'
+__install_requires__ = [
+]
